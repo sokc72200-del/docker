38c77df812477706ffa8ecfaba675d3a0e7d0247
Chan <sokc72200@gmail.com>
2026-07-06 09:23:54 +0700

    vue-router

--- vuejs-app/vite.config.ts
+++ vuejs-app/vite.config.ts
@@ -6,13 +6,23 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            protocol: 'ws',
+            port: 5173,
+        },
+        watch: {
+            usePolling: true,
+            useFsEvents: true,
+            interval: 1000,
+        },
+    },
 })
